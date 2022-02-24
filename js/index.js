1b58010abf32262cd4f383ef3c03f838ed3e9b27
Sebastián Orozco <44331089+Psicopatologia@users.noreply.github.com>
2022-02-24 00:16:36 -0500

    Pause video when not in screen

--- js/index.js
+++ js/index.js
@@ -1,10 +1,12 @@
 import MediaPlayer from "./MediaPlayer.js";
-import AutoPlay from "./plugins/Autoplay.js"
+import AutoPlay from "./plugins/AutoPlay.js";
+import AutoPause from "./plugins/AutoPause.js";
+
 
 const video = document.querySelector("video");
 const play = document.getElementById("play");
 const mute = document.getElementById("mute");
-const player = new MediaPlayer(video, [new AutoPlay()]);
+const player = new MediaPlayer(video, [new AutoPlay(), new AutoPause()]);
 
 play.onclick = () => player.togglePlay();
 mute.onclick = () => player.toggleMute();
